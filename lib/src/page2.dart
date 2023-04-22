ea8849a8aca68a90990fc2bae449e6f39cafe5d9
Biswajit kumar senapati <biswajitkumarsenapati2003@gmail.com>
2023-04-22 23:38:29 +0530

    2nd commit

--- lib/src/page2.dart
+++ lib/src/page2.dart
@@ -13,10 +13,11 @@ class page2 extends StatefulWidget {
 }
 
 class _page2State extends State<page2> {
+
+  var locationLoading = false;
   @override
   void initState() {
     getLocationInfo();
-    fetchWeatherInfo();
     super.initState();
   }
 
@@ -38,12 +39,37 @@ class _page2State extends State<page2> {
         tempMin: decodedJson["main"]["temp_min"],
         temp: decodedJson["main"]["temp"]);
   }
- void getLocationInfo() {
-  
- }
+
+  void getLocationInfo() async {
+    locationLoading = true;
+
+    await Geolocator.checkPermission();
+    await Geolocator.requestPermission();
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+
+    fetchWeatherInfo(long: position.longitude, lat: position.latitude);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+     body:  Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        margin: const EdgeInsets.only(top: 20),
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                            child: Text(
+                         Weather_modal.temp!,
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
+               
+    );
   }
 }
